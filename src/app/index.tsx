4f2a4a6400d3fc6186c3f95100bdf075cbf1c6c3
LUCAS FELIPE MARCELINO DA SILVA <lucas.fmsilva10@senacsp.edu.br>
2024-07-18 19:26:58 -0300

    new project

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/header";
+import { View, Text } from "react-native";
+
+
+export default function Home(){
+    return(
+        <View>
+            <Header title="faça seu pedido"/>
+            <Header title="sua sacolinha"/>
+        </View>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+
+module.exports = {
+     content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
+      theme: {
+        extend: {
+          fontFamily:{
+            heading:'Inter_600SemiBold',
+            subtitle: 'Inter_500Medium',
+            body:'Inter_400Regular', 
+            bold:'Inter_700Bold'
+        }
+      },
+      },
+      plugins: [],
+    }
