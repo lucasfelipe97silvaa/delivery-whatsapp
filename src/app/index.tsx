a09a9c4af5020f1dab9855629948b110f3adb9ff
LUCAS FELIPE MARCELINO DA SILVA <lucas.fmsilva10@senacsp.edu.br>
2024-07-22 22:14:08 -0300

    update projeto

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,48 @@
+import { useState } from "react";
+import { View, Text, FlatList, SectionList, } from "react-native";
+
+import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { View, Text } from "react-native";
+
+import { CATEGORIES, MENU } from "@/utils/data/products";
 
 
 export default function Home(){
+    const [category, setCategory] = useState(CATEGORIES[0])
+    function handleCategorySelect(selectedCategory: string){
+        setCategory(selectedCategory)
+        const sectionIndex = CATEGORIES.findIndex(category => category === selectedCategory)
+        console.log(selectedCategory)
+    }
+
     return(
         <View>
-            <Header title="faça seu pedido"/>
-            <Header title="sua sacolinha"/>
+            <Header title="faça seu pedido" cartQuantityItems={0}/>
+            <FlatList // Lista de dados 
+                data={CATEGORIES} keyExtractor={(item) => item}
+                renderItem={({item}) => <CategoryButton title={item} isSelected={item === category} onPress={()=> handleCategorySelect(item)}/> } horizontal className="max-h-14 mt-01"
+                showsHorizontalScrollIndicator={false} contentContainerStyle={{gap: 12, paddingHorizontal:20}}
+            />
+            <SectionList // Lista de dados categorizada, é o estilo do components basedo, no estilo de dado que possui
+                sections={MENU}
+                keyExtractor={(item) => item.id}
+                stickySectionHeadersEnabled={false} // não fazer o efeito de esticar
+                renderItem={({item}) => <Text className="text-white">{item.title}</Text>}
+                    renderSectionHeader={({section: {title} }) => (<Text className="text-yellow-200">{title}</Text>)}
+            />
+        
         </View>
     )
 }
+
+
+
+
+{/* 1° versão do flatList modo manual 
+
+ <View className="flex-row gap-8">
+<CategoryButton title="Lanche do dia" isSelected/>
+<CategoryButton title="Promoçoes"/>
+<CategoryButton title="Sobremesas"/>
+<CategoryButton title="Bebidas"/>
+</View> */}
---
+++ src/components/category-button.tsx
@@ -0,0 +1,17 @@
+import { Pressable, PressableProps, Text } from "react-native"; // Serve para o componente se tornar clicavel, além disso faz o scroll da tela
+import clsx from "clsx"; // faz mais de um elemento no mesmo css, tmb faz algumas condições dentro do css 
+
+type CategoryProps = PressableProps & {
+
+    title: string
+    isSelected?:boolean
+}
+
+export function CategoryButton({title, isSelected,...rest}: CategoryProps){
+    return(
+        <Pressable className={clsx("bg-slate-800 px-4 justify-center rounded-md h-10", isSelected && 'border-2 border-lime-300')}  {...rest}>
+
+            <Text className="text-slate-100  font-subtitle text-sm">{title}</Text>
+        </Pressable>
+    )
+}
